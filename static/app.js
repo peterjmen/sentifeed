8460e0b5914c8f0ccaff0d98cf4dc8644592ce37
InFatesHands <90697583+Infateshands@users.noreply.github.com>
2023-10-14 16:59:42 +1300

    final push

--- static/app.js
+++ static/app.js
@@ -1,36 +1,29 @@
+// var to hold open state
 var isOpen = false;
-articles =  document.getElementById("articles");
+// function for handling menu open/close
 function openNav() {
     
     if(isOpen === false){
         document.getElementById("filter").style.width = "80%";
-        // articles.style.marginLeft = "80%";
-        articles.style.filter = "blur(2px)";
         document.getElementById("mobile-header").style.marginLeft = "80%";
         isOpen = true
     }else {
         document.getElementById("filter").style.width = "0";
-        articles.style.marginLeft = "0";
-        articles.style.filter = "blur(0px)";
         document.getElementById("mobile-header").style.marginLeft = "0";
         isOpen = false
     }    
 }
 
-
 var modal = document.getElementById("myModal");
-
-
 var btn = document.getElementsByClassName("read-more-button");
-
-
 var span = document.getElementsByClassName("close")[0];
 
+// variables for storing selected article content
 var headline;
 var content;
 var url;
 
-
+// funtion for setting content in modal
 function setContent(head, con, link) {
     headline = head;
     content = con;
@@ -38,7 +31,7 @@ function setContent(head, con, link) {
     console.log(head)
 }
 
-
+// function for showing model
 function showModal() {
     document.getElementById("modalHeadline").innerHTML = headline
     document.getElementById("modalContent").innerHTML = content
@@ -46,11 +39,12 @@ function showModal() {
     modal.style.display = "block";
     
 }
-
+// close modal
 span.onclick = function() {
   modal.style.display = "none";
   
 }
+// close modal when clicking anywhere outside of modal
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
