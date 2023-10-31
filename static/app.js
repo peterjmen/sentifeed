cba234e1c56f83a761eaced0b57483195254dbeb
Peter Menhinick <peterjmen@gmail.com>
2023-10-31 18:26:15 +0700

    Added Feedback prompt for user-given rating submission
    No longer navigates away from homepage

--- static/app.js
+++ static/app.js
@@ -51,3 +51,39 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.getElementById('myModal');
+  const closeSpan = document.querySelector('.close');
+
+  closeSpan.addEventListener('click', function() {
+      modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
+  });
+
+  const evaluationForm = document.querySelector('.evaluation form');
+  evaluationForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+
+      // Make an AJAX request to submit the rating
+      const formData = new FormData(evaluationForm);
+
+      fetch('/submit-rating', {
+          method: 'POST',
+          body: formData,
+      })
+      .then(response => response.text())
+      .then(message => {
+          alert(message);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+          alert('There was an error submitting your evaluation.');
+      });
+  });
+});
